ab338e829f7d09baa13488a2c57a752ac5eea828
andersS91 <anders-soerensen@hotmail.com>
2017-09-23 09:30:00 +0200

    awdijlaw

---
+++ Framework/raytrace/Triangle.cpp
@@ -0,0 +1,121 @@
+// 02562 Rendering Framework
+// Written by Jeppe Revall Frisvad, 2011
+// Copyright (c) DTU Informatics 2011
+
+#include <optix_world.h>
+#include "HitInfo.h"
+#include "Triangle.h"
+
+using namespace optix;
+
+bool intersect_trianglex(const Ray& ray, 
+                        const float3& v0, 
+                        const float3& v1, 
+                        const float3& v2, 
+                        float3& n,
+                        float& t,
+                        float& v,
+                        float& w)
+{
+  // Implement ray-triangle intersection here (see Listing 1 in the lecture note).
+  // Note that OptiX also has an implementation, so you can get away
+  // with not implementing this function. However, I recommend that
+  // you implement it for completeness.
+
+	const float3 e1 = v1 - v0;
+	const float3 e2 = v2 - v0;
+	n = normalize(cross(e1, e2));
+
+
+	const float3 q = cross(ray.direction, e2);
+	
+	const float a = dot(e1, q);
+
+	const float3 s = ray.origin - v0;
+	const float3 r = cross(s, e1);
+
+
+
+	v = dot(s, q) / a;
+	w = dot(ray.direction, r) / a;
+
+	const float u = 1.0f - (v + w);
+	
+
+	t = dot(e2, r) / a;
+
+	static const float epsilon = 1e-6f;
+	static const float epsilon2 = 1e-10f;
+
+	if (a <= epsilon || u < -epsilon2 || v < -epsilon2 || w < -epsilon2 || t <= 0.0f) {
+		return false;
+	}
+	else {
+		return true;
+	}
+
+}
+
+
+bool Triangle::intersect(const Ray& r, HitInfo& hit, unsigned int prim_idx) const
+{
+  // Implement ray-triangle intersection here.
+  //
+  // Input:  r                    (the ray to be checked for intersection)
+  //         prim_idx             (index of the primitive element in a collection, not used here)
+  //
+  // Output: hit.has_hit          (set true if the ray intersects the triangle)
+  //         hit.dist             (distance from the ray origin to the intersection point)
+  //         hit.position         (coordinates of the intersection point)
+  //         hit.geometric_normal (the normalized normal of the triangle)
+  //         hit.shading_normal   (the normalized normal of the triangle)
+  //         hit.material         (pointer to the material of the triangle)
+  //        (hit.texcoord)        (texture coordinates of intersection point, not needed for Week 1)
+  //
+  // Return: True if the ray intersects the triangle, false otherwise
+  //
+  // Relevant data fields that are available (see Triangle.h)
+  // r                            (the ray)
+  // v0, v1, v2                   (triangle vertices)
+  // (t0, t1, t2)                 (texture coordinates for each vertex, not needed for Week 1)
+  // material                     (material of the triangle)
+  //
+  // Hint: Use the function intersect_triangle(...) to get the hit info.
+  //       Note that you need to do scope resolution (optix:: or just :: in front
+  //       of the function name) to choose between the OptiX implementation and
+  //       the function just above this one.
+
+	float3 n = make_float3(0.0f);
+	float tmark = 0.0f;
+	float v = 0.0f;
+	float w = 0.0f;
+
+	if(intersect_trianglex(r, v0, v1, v2, n, tmark, v, w)) {
+		tmark = fmaxf(tmark, 0.0f);
+		hit.has_hit = true;
+		hit.dist = tmark;
+		hit.position = r.origin + r.direction * tmark;
+		hit.geometric_normal = n;
+		hit.shading_normal = n;
+		hit.material = &material;
+		return true;
+	}
+
+  return false;
+}
+
+void Triangle::transform(const Matrix4x4& m) 
+{ 
+  v0 = make_float3(m*make_float4(v0, 1.0f)); 
+  v1 = make_float3(m*make_float4(v1, 1.0f)); 
+  v2 = make_float3(m*make_float4(v2, 1.0f)); 
+}
+
+Aabb Triangle::compute_bbox() const
+{
+  Aabb bbox;
+  bbox.include(v0);
+  bbox.include(v1);
+  bbox.include(v2);
+  return bbox;
+}
