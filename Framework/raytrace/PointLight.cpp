1f757f18bd72d7a7753076e72cadcb141f3f205a
andersS91 <anders-soerensen@hotmail.com>
2017-09-23 09:29:16 +0200

    alwjdawd

---
+++ Framework/raytrace/PointLight.cpp
@@ -0,0 +1,71 @@
+// 02562 Rendering Framework
+// Written by Jeppe Revall Frisvad, 2011
+// Copyright (c) DTU Informatics 2011
+
+#include <optix_world.h>
+#include "HitInfo.h"
+#include "mt_random.h"
+#include "PointLight.h"
+
+using namespace optix;
+
+bool PointLight::sample(const float3& pos, float3& dir, float3& L) const
+{
+  // Compute output and return value given the following information.
+  //
+  // Input:  pos (the position of the geometry in the scene)
+  //
+  // Output: dir (the direction toward the light)
+  //         L   (the radiance received from the direction dir)
+  //
+  // Return: true if not in shadow
+  //
+  // Relevant data fields that are available (see PointLight.h and Light.h):
+  // shadows    (on/off flag for shadows)
+  // tracer     (pointer to ray tracer)
+  // light_pos  (position of the point light)
+  // intensity  (intensity of the emitted light)
+  //
+  // Hint: Construct a shadow ray using the Ray datatype. Trace it using the
+  //       pointer to the ray tracer.
+	if (shadows) {
+		Ray& shadowRay = Ray(pos, normalize(light_pos - pos), 0, 1.0e-4f, length(light_pos - pos));
+		HitInfo hit;
+
+		if (tracer->trace_to_any(shadowRay, hit)) {
+			return false;
+		}
+	}
+
+	const float3 ab = light_pos - pos;
+	dir = normalize(ab);
+	L = intensity / pow(length(ab),2);
+	return true;
+}
+
+bool PointLight::emit(Ray& r, HitInfo& hit, float3& Phi) const
+{
+  // Emit a photon by creating a ray, tracing it, and computing its flux.
+  //
+  // Output: r    (the photon ray)
+  //         hit  (the photon ray hit info)
+  //         Phi  (the photon flux)
+  //
+  // Return: true if the photon hits a surface
+  //
+  // Relevant data fields that are available (see PointLight.h and Light.h):
+  // tracer     (pointer to ray tracer)
+  // light_pos  (position of the point light)
+  // intensity  (intensity of the emitted light)
+  //
+  // Hint: When sampling the ray direction, use the function
+  //       mt_random() to get a random number in [0,1].
+
+  // Sample ray direction and create ray
+
+  // Trace ray
+  
+  // If a surface was hit, compute Phi and return true
+
+  return false;
+}
